6b046b23b6acd4d77a9929b44ab87c0f523b1593
Victor Allen <vamuigua@gmail.com>
2017-07-17 15:24:20 +0300

    Added routes for index, success, and tasks

---
+++ app.rb
@@ -0,0 +1,31 @@
+require("sinatra")
+require("sinatra/reloader")
+require("sinatra/activerecord")
+also_reload("lib/**/*.rb")
+require("./lib/task")
+require("pg")
+
+get('/') do
+  @tasks = Task.all()
+  erb(:index)
+end
+
+post('/tasks') do
+  description =params.fetch('description')
+  task = Task.new({:description => description, :done => false})
+  task.save()
+  erb(:success)
+end
+
+get('/tasks/:id/edit') do
+  @task = Task.find(params.fetch("id").to_i())
+  erb(:task_edit)
+end
+
+patch("/tasks/:id") do
+  description = params.fetch("description")
+  @task = Task.find(params.fetch("id").to_i())
+  @task.update({:description => description})
+  @tasks = Task.all()
+  erb(:index)
+end
