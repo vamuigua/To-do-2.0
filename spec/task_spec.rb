760488ceba767a8c95728b9a0066d1233cda6d85
Victor Allen <vamuigua@gmail.com>
2017-07-17 17:16:40 +0300

    Added spec to tell us where a task belongs to

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,14 +1,19 @@
 require("spec_helper")
 
 describe(Task) do
-
-  describe(".not_done") do
-    it("returns the not done tasks") do
-      not_done_task1 = Task.create({:description => "gotta do it", :done => false})
-      not_done_task2 = Task.create({:description => "gotta do it too", :done => false})
-      not_done_tasks = [not_done_task1, not_done_task2]
-      done_task = Task.create({:description => "done task", :done => true})
-      expect(Task.not_done()).to(eq(not_done_tasks))
+  describe("#list") do
+    it("tells which list it belongs to") do
+      test_list = List.create({:name => "list"})
+      test_task = Task.create({:description => "task", :list_id => test_list.id})
+      expect(test_task.list()).to(eq(test_list))
+    end
+    describe(".not_done") do
+      it("returns the not done tasks") do
+        not_done_task1 = Task.create({:description => "gotta do it", :done => false})
+        not_done_task2 = Task.create({:description => "gotta do it too", :done => false})
+        not_done_tasks = [not_done_task1, not_done_task2]
+        done_task = Task.create({:description => "done task", :done => true})
+        expect(Task.not_done()).to(eq(not_done_tasks))
+      end
     end
   end
-end
