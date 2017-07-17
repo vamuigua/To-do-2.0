75205402ae8141b31f8c76e49504e479d362b565
Victor Allen <vamuigua@gmail.com>
2017-07-17 15:26:21 +0300

    Added specs for tasks not done

---
+++ spec/task_spec.rb
@@ -0,0 +1,14 @@
+require("spec_helper")
+
+describe(Task) do
+
+  describe(".not_done") do
+    it("returns the not done tasks") do
+      not_done_task1 = Task.create({:description => "gotta do it", :done => false})
+      not_done_task2 = Task.create({:description => "gotta do it too", :done => false})
+      not_done_tasks = [not_done_task1, not_done_task2]
+      done_task = Task.create({:description => "done task", :done => true})
+      expect(Task.not_done()).to(eq(not_done_tasks))
+    end
+  end
+end
