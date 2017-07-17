af268257e0ad921ff526bb424fd8f65f4e0fa325
Victor Allen <vamuigua@gmail.com>
2017-07-17 17:23:49 +0300

    Added method to validate if description is true

--- lib/task.rb
+++ lib/task.rb
@@ -1,5 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to(:list)
+  validates(:description, :presence => true)
 
   #method to return a query from the DB of tasks not done
   scope(:not_done, -> do
