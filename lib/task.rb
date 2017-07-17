dc435bb5f40a7bbece6ae25a3ce93ec30bfa1ca5
Victor Allen <vamuigua@gmail.com>
2017-07-17 17:27:27 +0300

    Added validation method to check if description is longer than 50 characters

--- lib/task.rb
+++ lib/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to(:list)
-  validates(:description, :presence => true)
+  validates(:description, {:presence => true, :length => { :maximum => 50 }})
 
   #method to return a query from the DB of tasks not done
   scope(:not_done, -> do
