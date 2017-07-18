cf3bc85be6c297a0a0c4528e83b65325f18a3236
Victor Allen <vamuigua@gmail.com>
2017-07-18 12:49:57 +0300

    Added a redirect route on saving a new_task

--- app.rb
+++ app.rb
@@ -1,22 +1,28 @@
 #used bundler to require all gems
 require("bundler/setup")
 #__FILE__ refers to the current file, and File.dirname(__FILE__) will give the current directory
-Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 Bundler.require(:default)
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
   @tasks = Task.all()
   erb(:index)
 end
 
-post('/tasks') do
-  description =params.fetch('description')
-  @task = Task.new({:description => description, :done => false})
-  if @task.save()
-    erb(:success)
-  else
-    erb(:errors)
-  end
+#post request to post new_task created, save task and redirect to new_task
+post ('/tasks')
+@new_task = Task.new(params.fetch("task"))
+if @new_task.save()
+  redirect("/tasks/".concat(@new_task.id().to_s()))
+else
+  erb(:index)
+end
+end
+
+#get the new_task created
+get('/tasks/:id') do
+  @task = Task.find(params.fetch("id").to_i())
+  erb(:task)
 end
 
 get('/tasks/:id/edit') do
