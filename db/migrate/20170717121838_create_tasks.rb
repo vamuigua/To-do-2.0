f4a775642f9127496957bfc2a3cda62f0c70cde0
Victor Allen <vamuigua@gmail.com>
2017-07-18 12:02:31 +0300

    Updated migrations

--- db/migrate/20170717121838_create_tasks.rb
+++ db/migrate/20170717121838_create_tasks.rb
@@ -1,5 +1,6 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
+    #create a table called tasks
     create_table(:tasks) do |t|
       t.column(:description, :string)
       t.column(:done, :boolean)
--- db/migrate/20170717124329_create_lists.rb
+++ db/migrate/20170717124329_create_lists.rb
@@ -1,11 +1,13 @@
 class CreateLists < ActiveRecord::Migration[5.1]
   def change
+    #create a table called lists
     create_table(:lists) do |t|
       t.column(:name, :string)
 
       t.timestamps()
     end
 
+    #add a column in the tasks table called list_id with type interger
     add_column(:tasks, :list_id, :integer)
   end
 end
