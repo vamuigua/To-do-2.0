124ecb867b30c3cf9ca9efee5906830a849a944b
Victor Allen <vamuigua@gmail.com>
2017-07-19 14:29:47 +0300

    Modified tests

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -2,11 +2,9 @@ require('spec_helper')
 
 describe(List) do
   describe("#tasks") do
-    it("tells which tasks are in it") do
-      test_list = List.create({:name => "list"})
-      test_task1 = Task.create({:description => "task1", :list_id => test_list.id})
-      test_task2 = Task.create({:description => "task2", :list_id => test_list.id})
-      expect(test_list.tasks()).to(eq([test_task1, test_task2]))
-    end
+    it { should tell us how many tasks are recorded(:tasks) }
+  end
+  describe("tasks") do
+    it { should have_many(:tasks) }
   end
 end
